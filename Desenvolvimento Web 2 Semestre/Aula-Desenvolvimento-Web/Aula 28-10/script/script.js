7630ccaa568a77b1b614fa4b837d726022256b41
RodrigoDGFerreira <123335370+RodrigoDGFerreira@users.noreply.github.com>
2024-10-31 16:03:58 -0300

    Add files via upload

---
+++ Desenvolvimento Web 2 Semestre/Aula-Desenvolvimento-Web/Aula 28-10/script/script.js
@@ -0,0 +1,6 @@
+function ligar(){
+    document.getElementById('lamp').src="https://www.abilumi.org.br/wp-content/uploads/2017/02/abilumi-fim-de-lampada-incadescente-300x402.jpg"
+}
+function desligar(){
+    document.getElementById('lamp').src="https://cdn.sistemawbuy.com.br/arquivos/f3e22f7ffd7995387ae66f8741b64c00/produtos/640a48d42d0bd/lamp-incand-ecologena-640a48d470a8e.png";
+}
